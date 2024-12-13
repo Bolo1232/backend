551f9e0c721b437827d6662ead0aadf5ecccb6d9
Xevery Jan Bolo <140127935+Bolo1232@users.noreply.github.com>
2024-12-13 21:20:44 +0800

    Update GenreRepository.java

---
+++ src/main/java/wildtrack/example/wildtrackbackend/repository/GenreRepository.java
@@ -0,0 +1,10 @@
+package wildtrack.example.wildtrackbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import wildtrack.example.wildtrackbackend.entity.Genre;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
