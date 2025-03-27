71cd1e6b869d2debc1f887956eadbe3736b31a28
Xevery Jan Bolo <140127935+Bolo1232@users.noreply.github.com>
2025-03-27 16:25:09 +0800

    Update WebConfig.java

--- src/main/java/wildtrack/example/wildtrackbackend/config/WebConfig.java
+++ src/main/java/wildtrack/example/wildtrackbackend/config/WebConfig.java
@@ -1,12 +1,13 @@
 package wildtrack.example.wildtrackbackend.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,5 +22,17 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations(uploadLocation);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:3000", 
+                "https://wild-track.vercel.app/" // Replace with your actual Vercel frontend URL
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
